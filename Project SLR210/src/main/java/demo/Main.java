908d3ffb6348b851b1a867d8d9ff2ab935d95524
TheoMondou <32342277+TheoMondou@users.noreply.github.com>
2020-06-05 10:14:33 +0200

    correct indentation

--- Project SLR210/src/main/java/demo/Main.java
+++ Project SLR210/src/main/java/demo/Main.java
@@ -50,12 +50,12 @@ public class Main {
         // We wait 5 seconds before ending system (by default)
 	    // But this is not the best solution.
 	    try {
-			waitBeforeTerminate();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		} finally {
-			system.terminate();
-		}
+			  waitBeforeTerminate();
+      } catch (InterruptedException e) {
+          e.printStackTrace();
+      } finally {
+          system.terminate();
+      }
     }
     public static void waitBeforeTerminate() throws InterruptedException {
 		Thread.sleep(5000);
